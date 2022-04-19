e141648618506781c6ef70076b8c9f66e2ea4a56
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-04-19 18:11:17 +0200

    calculate function

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const amountOne = document.querySelector('.amount-one');
 const amountTwo = document.querySelector('.amount-two');
 const swapBtn = document.querySelector('.swap')
 const rateInfo = document.querySelector('.rate-info')
+
+const calculate = () => {
+    fetch(`https://api.exchangerate.host/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const currency1 = currencyOne.value;
+            const currency2 = currencyTwo.value;
+
+            const rate = data.rates[currency2];
+        })
+}
